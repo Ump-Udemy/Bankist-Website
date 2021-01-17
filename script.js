72de0560988fb311d34b2304b585a5e4c93be47a
Umpz <46953365+PeerawatUmp@users.noreply.github.com>
2021-01-18 00:15:53 +0700

    implement smooth scrolling

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const section2 = document.querySelector('#section--2');
 
 const openModal = (event) => {
   event.preventDefault();
@@ -21,12 +24,23 @@ const closeModal = () => {
 
 btnsOpenModal.forEach((btn)=> btn.addEventListener('click', openModal));
 
-
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
+
+btnScrollTo.addEventListener('click',(event)=>{
+  const s1coord = section1.getBoundingClientRect();
+  
+  // window.scrollTo({
+  //   left:s1coord.left + window.pageXOffset,
+  //   top:s1coord.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  
+  section1.scrollIntoView({behavior: 'smooth'});
+})
