78ca7a0ec7d61a2f5e5e007dc3e7dd25d4ed8b3d
Umpz <46953365+PeerawatUmp@users.noreply.github.com>
2021-01-17 15:03:01 +0700

    changing to arrow function and using forEach instead of for loop

--- script.js
+++ script.js
@@ -8,18 +8,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = (event) => {
+  event.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach((btn)=> btn.addEventListener('click', openModal));
+
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
