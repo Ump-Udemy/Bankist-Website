18c08495ceb0c657e4c1ed76f66027943723bef5
Umpz <46953365+PeerawatUmp@users.noreply.github.com>
2021-01-19 21:40:26 +0700

    implement reveal section effect

--- script.js
+++ script.js
@@ -90,3 +90,19 @@ const stickyNav = (entries) => {
 
 const headerObserver = new IntersectionObserver(stickyNav,{root: null, threshold: 0, rootMargin: '-90px'});
 headerObserver.observe(header);
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = (entries, observer) => {
+  const entry = entries[0];
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection,{root: null, threshold: 0.15});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
